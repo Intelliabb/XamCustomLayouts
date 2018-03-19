4cd9ec2f35ae11031864ddd5dd6d942c1d36875a
Hussain Abbasi <intelliabb@live.com>
2018-03-18 23:21:04 -0500

    Initial Commit

---
+++ XamCustomLayouts/XamCustomLayouts/XamCustomLayoutsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamCustomLayouts
+{
+    public partial class XamCustomLayoutsPage : ContentPage
+    {
+        public XamCustomLayoutsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
